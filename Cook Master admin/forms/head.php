6c6a8f7b2df3b0cd0bb87dbe58bb61139ec5e487
Gilberto <gilbertofilho_fr@hotmail.com>
2023-05-02 14:58:51 +0200

    Avancement sur le bakc-office

---
+++ Cook Master admin/forms/head.php
@@ -0,0 +1,40 @@
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Cook Master</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/CM.svg" rel="icon">
+  <link href="assets/img/CM.svg" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Gabriela&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- Inclure la bibliothèque JavaScript de Google Translate -->
+  <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit&key=AIzaSyBYVO-4yzlFzMfEc66ACzSqWhc9I52oPmQ"></script>
+
+  <!-- Définir la fonction d'initialisation -->
+  <script type="text/javascript">
+  function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'fr', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+  }
+  </script>
+
+<!-- AIzaSyBYVO-4yzlFzMfEc66ACzSqWhc9I52oPmQ -->
+
+</head>
--- Cook Master/index.php
+++ Cook Master/index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,29 +28,7 @@
     </div>
   </div>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
-
-      <h1 class="logo me-auto me-lg-0"><a href="index.html">Cook Master</a></h1>
-
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Accueil</a></li>
-          <li><a class="nav-link scrollto" href="#why-us">Abonnement</a></li>
-          <li><a class="nav-link scrollto" href="#menu">Recette</a></li>
-          <li><a class="nav-link scrollto" href="#events">Formation</a></li>
-          <li><a class="nav-link scrollto" href="#book-a-table">Réservations</a></li>
-          <li><a class="nav-link scrollto" href="#">Matériels</a></li>
-          <li><a class="nav-link scrollto" href="#gallery">Galerie</a></li>
-          <li><a class="nav-link scrollto" href="#chefs">Chefs</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-      <a href="page.connexion.php" class="book-a-table-btn scrollto d-none d-lg-flex">Connexion</a>
-    </div>
-  </header><!-- End Header -->
+  <?php require_once "forms/header.php"; ?>
 
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
